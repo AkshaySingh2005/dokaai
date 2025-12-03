9bcde70cc961f378b3ec3a80738f000990cf9532
AkshaySingh2005 <akshaysengar15671@google.com>
2025-12-03 20:45:02 +0530

    test successfull

--- src/prisma/prismaClient.ts
+++ src/prisma/prismaClient.ts
@@ -3,13 +3,13 @@ import { Pool } from "pg";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
 
-// Single shared pg pool
+
 const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 const adapter = new PrismaPg(pool);
 
 export const prisma = new PrismaClient({ adapter });
 
-// Graceful shutdown
+
 process.on("SIGINT", async () => {
   await prisma.$disconnect();
   await pool.end();
